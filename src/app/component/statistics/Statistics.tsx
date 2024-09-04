45f2638510ae927a0f62589df5b8402b739f3bc0
diamond nathaniel <diamondnathaniel2@gmail.com>
2024-09-04 10:16:58 +0100

    mobile stats view

--- src/app/component/statistics/Statistics.tsx
+++ src/app/component/statistics/Statistics.tsx
@@ -8,20 +8,22 @@ const Statistics = () => {
   return (
     <div className="bg-black pt-[7%] px-[15%] mt-[3%] pb-[5%]">
       <CustomHeader bgCol="white" title="my statistics" color="#fff" />
-      <div className="flex gap-2">
-        <div className="w-[70%]">
-          <h1 className="text-white max-w-[80%] text-5xl font-semibold leading-none mt-10">
+      <div className="flex flex-col md:flex-row gap-2">
+        <div className="w-[100%] md:w-[70%]">
+          <h1 className="text-white md:max-w-[80%] leading-normal text-2xl md:text-5xl font-semibold md:leading-none mt-2 md:mt-10">
             My skill analytics as a Product designer
           </h1>
-          <p className="text-white max-w-[80%] mt-4 font-normal leading-normal">
+          <p className="text-white text-left mt-2 md:text-left text-sm md:text-base md:max-w-[80%] md:mt-4 font-normal leading-normal">
             As a product designer, I craft user-centered solutions that blend
             functionality with aesthetics, ensuring products are both innovative
             and aligned with business goals. My focus is on creating seamless,
             impactful experiences from concept to completion.
           </p>
-          <StatsData />
+          <div className="hidden md:block">
+            <StatsData />
+          </div>
         </div>
-        <div className="w-[30%]">
+        <div className="w-[90%] flex flex-col mt-4 md:mt-0 self-center md:w-[30%]">
           <img
             src="./images/pieChartImg.png"
             alt="pieChart"
@@ -29,6 +31,9 @@ const Statistics = () => {
             height="100%"
             className="rotate-animation"
           />
+          <div className="md:hidden">
+            <StatsData />
+          </div>
         </div>
       </div>
     </div>
--- src/app/component/statistics/StatsData.tsx
+++ src/app/component/statistics/StatsData.tsx
@@ -4,26 +4,28 @@ import DotIcon from '../../../../public/customIcon/DotIcon';
 const StatsData = () => {
   return (
     <div className="mt-[3%]">
-      <div className="flex  flex-wrap items-center gap-4  max-w-[70%]">
+      <div className=" grid grid-cols-2	truncate gap-4 mt-4 md:flex  md:flex-wrap md:items-center md:gap-4   md:max-w-[70%]">
         <div className="flex items-center gap-2">
           <DotIcon />
-          <h3 className="text-white">Visual Design</h3>
+          <h3 className="text-white text-sm md:text-base">Visual Design</h3>
         </div>
         <div className="flex items-center gap-2">
           <DotIcon initialColour="#AC94F1" />
-          <h3 className="text-white">Interactive Design</h3>
+          <h3 className="text-white text-sm md:text-base">
+            Interactive Design
+          </h3>
         </div>
         <div className="flex items-center gap-2">
           <DotIcon initialColour="#FFF0CA" />
-          <h3 className="text-white">Motion Design</h3>
+          <h3 className="text-white text-sm md:text-base ">Motion Design</h3>
         </div>
         <div className="flex items-center gap-2">
           <DotIcon initialColour="#F9CF64" />
-          <h3 className="text-white">User research</h3>
+          <h3 className="text-white text-sm md:text-base">User research</h3>
         </div>
         <div className="flex items-center gap-2">
           <DotIcon initialColour="#F38FBF" />
-          <h3 className="text-white">Prototyping</h3>
+          <h3 className="text-white text-sm md:text-base">Prototyping</h3>
         </div>
       </div>
     </div>
