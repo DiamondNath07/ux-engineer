aead3cb281f4f46ffb9814b45bd6396be9db5e54
diamond nathaniel <diamondnathaniel2@gmail.com>
2024-09-04 12:25:51 +0100

    add updadtedd

--- src/app/component/button/index.jsx
+++ src/app/component/button/index.jsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react';
 
-const Button = ({ label, onClick, width, text = 'base', px = 8, py = 3 }) => {
+const Button = ({ label, onClick, width, text = 'base' }) => {
   return (
     <button
       onClick={onClick}
       type="button"
-      className={`bg-[#1D2130] hover:bg-[#525560] text-white  px-${px} py-${py} rounded-md cursor-pointer text-${text}`}
+      className={`bg-[#1D2130] hover:bg-[#525560] text-white  px-8 py-3 rounded-md cursor-pointer text-${text}`}
     >
       {label}
     </button>
